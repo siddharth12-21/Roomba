e56cedaf2368a4e5fa9bf67e76c5652beabe1e33
siddharth12-21 <siddharth.ramesh1221@gmail.com>
2025-08-29 09:26:42 -0700

    Add files via upload

--- RoomCleaner/Driver.java
+++ RoomCleaner/Driver.java
@@ -9,43 +9,42 @@ public class Driver implements Directions {
 // It will be assigned a value in the getInfo method
 	private static Robot roomba; 
 
-
-	// You will add very many variables!!
-
-	
 	public static void main(String[] args) {
 		// LEAVE THIS ALONE!!!!!!
 		Driver d = new Driver();
-
-  /**
-	 * This section of code gets info from the user in the following order: 1. Ask the user
-	 * which world file they wish to process. Right now, that world file name is
-	 * hardcoded in. 2. Ask the user for the starting location and direction of the
-	 * Robot. A new Robot should be constructed and assigned to the global
-	 * (instance) variable named roomba that is declared on line 10.
-	 * 
-	 * An inelegant way to interact with the user is via the console, using
-	 * System.out.println and a Scanner that scans System.in (input from the
-	 * console). A more elegant way to get user input might include using a
-	 * JOptionPane.
-	 */
+		Robot roomba = new Robot(7,6,North,0);
 
     String wrldName = "basicRoom.wld";
-
-		World.readWorld(wrldName);
+	World.readWorld(wrldName);
     World.setVisible(true);
 
-    
+	int turner= 0;
 
-
-	/** This section will have all the logic that takes the Robot to every location 
-	 * and cleans up all piles of beepers.  Think about ways you can break this
-	 * large, complex task into smaller, easier to solve problems.
-	 */
+	while (roomba.frontIsClear()==true){
+		roomba.move();
+		while (roomba.frontIsClear()==false){
+			if (turner/2==0) {
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.move();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+			}
+			if(turner/2!=0){
+				roomba.turnLeft();
+				roomba.move();
+				roomba.turnLeft();
+			}
+			turner++;
+		}
+	}
 
 		// the line below causes a null pointer exception
 		// what is that and why are we getting it?
-		roomba.move();
+
+		
 
 
   
@@ -54,8 +53,7 @@ public class Driver implements Directions {
 
 
 
-  	/** This method displays the results of cleaning the room.  All of the info
-	 * in the pdf that describes the problem need to be displayed.  You can present
+  	/*
 	 * this info in the console (boring) or you can present using JOptionPane (cool!)
 	 */
 
